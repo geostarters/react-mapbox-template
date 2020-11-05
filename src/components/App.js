e4f8a7235ef79fb9480c54f366607e1732c26919
Wladimir Szczerban <bolosig@gmail.com>
2020-11-05 12:35:47 +0100

    simple template

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,35 @@
 // @flow
 
 import React, { Component } from "react";
-import logo from "../logo.svg";
 import styles from "./App.module.css";
-import { Map, LogoICGC } from "@geostarters/react-components";
+import { Map, LogoICGC, Header, Footer } from "@geostarters/react-components";
+import IMAGES from "../resources/images";
 
 class App extends Component {
 
 	render() {
 
-		return (
-			<div className="App">
+		/**/
 
-				<header className={styles.AppHeader}>
-					<a className={styles.AppLink} href="https://github.com/geostarters/react-project-template" target="_blank" rel="noopener noreferrer" >
-            ICGC react template
-					</a>
-				</header>
+		return (
+			<div className={styles.App}>
+
+				<div className={styles.header}>
+					<Header title="Map Template" pathLogo={IMAGES.GENCAT_white} logoSize="medium" customStyleBar={{backgroundColor: "var(--primary-color)", color: "var(--primary-white)"}} showRightElements={true} customStyleLogo={{maxWidth: "15em"}}>
+						<div className={styles.flexRow}>
+							<div className={styles.logoText}>ICGC</div>
+							<LogoICGC position="top-right"/>
+						</div>
+					</Header>
+				</div>
 
 				<div className={styles.AppBody}>
 					<div className={styles.containerMap}>
 						<Map showAttribution={false}></Map>
 					</div>
-					<img src={logo} className={styles.AppLogo} alt="logo" />
 				</div>
 
-				<LogoICGC className={styles.AppLogo}></LogoICGC>
+				<Footer title="by @geostarters" customStyleBar={{position: "relative", marginTop: 0}}></Footer>
 
 			</div>
 		);
---
+++ src/resources/images.js
@@ -0,0 +1,8 @@
+// @flow
+
+const IMAGES = {
+	"ICGC_white": require("./images/ICGC_white.svg"),
+	"GENCAT_white": require("./images/NG_logo_generalitat.png")
+};
+
+export default IMAGES;
